ae299a5c0926af59cba36cb3248683efac82dace
Neoteuthis <Neoteuthis@users.noreply.github.com>
2018-10-22 17:10:22 -0400

    worked on more platforms

---
+++ Spookyrun/Assets/Scripts/objectpooler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+[System.Serializable]
+public struct ObjectPoolItem
+{
+    public int amountToPool;
+    public GameObject objectToPool;
+    public bool shouldExpand;
+}
+public class objectpooler : MonoBehaviour
+{
+    public List<ObjectPoolItem> itemsToPool;
+    //  public bool shouldExpand = true;
+    public static objectpooler SharedInstance;
+    public List<GameObject> pooledObjects;
+    //   public GameObject objectToPool;
+    //  public int amountToPool;
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        foreach (ObjectPoolItem item in itemsToPool)
+        {
+            for (int i = 0; i < item.amountToPool; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(item.objectToPool);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+            }
+        }
+    }
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+    public GameObject GetPooledObject(string tag)
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy && pooledObjects[i].tag == tag)
+            {
+                return pooledObjects[i];
+            }
+        }
+        foreach (ObjectPoolItem item in itemsToPool)
+        {
+            if (item.objectToPool.tag == tag)
+            {
+                if (item.shouldExpand)
+                {
+                    GameObject obj = (GameObject)Instantiate(item.objectToPool);
+                    obj.SetActive(false);
+                    pooledObjects.Add(obj);
+                    return obj;
+                }
+            }
+        }
+        return null;
+    }
+}
---
+++ Spookyrun/Assets/Scripts/summonscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class summonscript : MonoBehaviour {
+    public levelmanager mything;
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       mything.spawnNewRoom();
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
